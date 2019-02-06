7e148e8a6833e03d0e87fdda9dcebc23bedf2bce
VIncent Pacelli <vpacelli@princeton.edu>
2019-02-06 16:18:38 -0500

    Changed existing control problems to use new probability framework. Implemented value iteration (untested).

--- Python/trcontrol/framework/control/control_problem.py
+++ Python/trcontrol/framework/control/control_problem.py
@@ -1,10 +1,8 @@
 import numpy as np
 from abc import ABC, abstractmethod
-from typing import Union
-from scipy.stats import rv_discrete, rv_continuous
 from trcontrol.framework.filter.discrete import DiscreteFilter
-
-
+import trcontrol.framework.prob.dists as dists
+import trcontrol.framework.prob.channels as channels
 
 
 class ControlProblem(ABC):
@@ -12,11 +10,14 @@ class ControlProblem(ABC):
         pass
 
     @abstractmethod
-    def simulate(self, init: Union[rv_discrete, rv_continuous], horizon: int) -> (np.ndarray, np.ndarray):
+    def simulate(self, init: dists.Distribution, horizon: int) -> (np.ndarray, np.ndarray):
         pass
+
 
 class DSCProblem(ControlProblem):
     def __init__(self):
+        super().__init__()
+
         self._dynamics = self.create_dynamics()
         (n, m, _) = self._dynamics.shape
 
@@ -24,7 +25,7 @@ class DSCProblem(ControlProblem):
         self._sensor = self.create_sensor()
         self._costs = self.create_costs()
         self._terminal_costs = self.create_terminal_costs()
-        self._state_dist = np.empty()
+        self._state_dist = None
 
     @abstractmethod
     def create_dynamics(self) -> np.ndarray:
@@ -66,27 +67,47 @@ class DSCProblem(ControlProblem):
     def state_dist(self) -> np.ndarray:
         return self._state_dist
 
-    def simulate(self, init_dist: rv_discrete, horizon: int) -> (np.ndarray, np.ndarray):
+    def simulate(self, init_dist: dists.FiniteDist, horizon: int) -> (np.ndarray, np.ndarray):
         traj = np.zeros(horizon + 1)
         costs = np.zeros(horizon + 1)
-        (n_outputs, n_states) = self.sensor.shape
+        sensor_channel = channels.DiscreteChannel(self.sensor)
 
-        traj[0] = init_dist.rvs()
-        meas = rv_discrete(values=(range(n_outputs), self.sensor[:, traj[0]])).rvs()
+        traj[0] = init_dist.sample()
+        meas = sensor_channel.marginal(traj[0]).sample()
 
         bf = DiscreteFilter(self, init_dist, meas)
 
         for t in range(horizon):
-            input_dist = rv_discrete(values=(range(n_outputs), self.policy[:, bf.mle]))
-            input = input_dist.rvs()
+            input_dist = dists.FiniteDist(self.policy[:, bf.mle()])
+            input = input_dist.sample()
             costs[t] = self.costs[traj[t], input]
 
-            next_state_dist = rv_discrete(values=(range(n_outputs), self.dynamics[:, traj[t], input]))
-            traj[t + 1] = next_state_dist.rvs()
+            next_state_dist = dists.FiniteDist(self.dynamics[:, traj[t], input])
+            traj[t + 1] = next_state_dist.sample()
 
-            meas = rv_discrete(values=(range(n_outputs), self.sensor[:, traj[t + 1]])).rvs()
-            bf.iterate(self.state_dist[:, t + 1], meas)
+            meas = sensor_channel.marginal(traj[t + 1])
+            bf.iterate(meas)
 
-        costs[horizon + 1] = costs[t] = self.terminal_costs[traj[horizon + 1]]
+        costs[horizon + 1] = self.terminal_costs[traj[horizon + 1]]
 
-        return (traj, costs)
+        return traj, costs
+
+    def solve_value_iter(self, iters: int = 100) -> (np.ndarray, np.ndarray):
+        costs = self.costs
+        dynamics = self.dynamics
+        (n, _, m) = dynamics.shape
+
+        values = np.zeros(n)
+        input_idx = np.zeros(n)
+        policy = np.zeros(m, n)
+
+        for itr in range(iters):
+            for i in range(n):
+                possible_future_costs = values @ dynamics[:, i, :] + costs[i, :]
+                input_idx[i] = possible_future_costs.argmin()
+                values[i] = possible_future_costs[input_idx]
+
+        for i in range(n):
+            policy[input_idx[i], i] = 1
+
+        return policy, values
