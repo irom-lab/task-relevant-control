3c3b24c32cc72ca13064a01bd7e8232f98142557
Vincent Pacelli <vpacelli@princeton.edu>
2019-02-07 17:31:25 -0500

    Changed type descriptor of argument meas of method iterate to correct type.

--- Python/trcontrol/framework/filter/bayes.py
+++ Python/trcontrol/framework/filter/bayes.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class BayesFilter(ABC):
-    def __init__(self, init_dist: dists.Distribution, init_meas: Union[int, np.ndarray]) -> None:
+    def __init__(self, init_dist: dists.Distribution, init_meas: dists.Distribution) -> None:
         self._belief = self.measurement_update(init_dist, init_meas)
 
     def belief(self) -> dists.Distribution:
@@ -16,13 +16,13 @@ class BayesFilter(ABC):
         pass
 
     @abstractmethod
-    def measurement_update(self, proc_belief: dists.Distribution, meas: Union[int, np.ndarray]) -> dists.Distribution:
+    def measurement_update(self, proc_belief: dists.Distribution, meas: dists.Distribution) -> dists.Distribution:
         pass
 
     @abstractmethod
     def mle(self):
         pass
 
-    def iterate(self, meas: Union[int, np.ndarray]):
+    def iterate(self, meas: dists.Distribution):
         proc_belief = self.process_update(self._belief)
         self._belief = self.measurement_update(proc_belief, meas)
