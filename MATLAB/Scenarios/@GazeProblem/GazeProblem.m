896f188e88a491b34ce7b7b277fe66c80289446f
tnecniv <vpacelli@princeton.edu>
2019-04-09 12:45:38 -0400

    Now a continuous state problem.

--- MATLAB/Scenarios/@GazeProblem/GazeProblem.m
+++ MATLAB/Scenarios/@GazeProblem/GazeProblem.m
@@ -1,4 +1,4 @@
-classdef GazeProblem < DiscControlProblem
+classdef GazeProblem < ContControlProblem
     %GAZEPROBLEM Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -6,137 +6,49 @@ classdef GazeProblem < DiscControlProblem
     end
     
     methods
-        function obj = GazeProblem(Parameters, SolverOptions, varargin)
-            obj = obj@DiscControlProblem(Parameters, SolverOptions, varargin{:});
+        function obj = GazeProblem(Parameters, SolverOptions, Init, varargin)
+            obj = obj@ContControlProblem(Parameters, SolverOptions, Init, varargin{:});
+            
+            obj.Parameters.NStates = 7;
+            obj.Parameters.NInputs = 1;
+        end
+    end
+    
+    methods
+        function next_state = dynamics(Obj, State, Input, t)
+            next_state = State + Obj.Parameters.DeltaT * [State(4:6); Input; 0; -Obj.Parameters.Gravity; 0];
         end
         
-        
-        function [costs, terminal_costs] = costs(Obj)
-            runway = Obj.Parameters.Runway;
-            height = Obj.Parameters.Height;
+        function [A, B] = linearize(Obj, State, Input, t)
+            dt = Obj.Parameters.DeltaT;
+            g = Obj.Parameters.Gravity;
             
-            n = runway * runway * height * 2;
-            m = 5;
-            
-            costs = [ones(n, 4) zeros(n, 1)];
-            terminal_costs = zeros(n, 1);
-            
-            for x = 1:runway
-                for ballx = 1:runway
-                    for ballvel = 1:2
-                        if ballx ~= x
-                            state_ind = sub2ind([runway runway height 2], x, ballx, 1, ballvel);
-                            terminal_costs(state_ind) = 0;
-                        else
-                            state_ind = sub2ind([runway runway height 2], x, ballx, 1, ballvel);
-                            terminal_costs(state_ind) = -20;
-                        end
-                    end
-                end
-            end
+            A = [1 0 0 dt 0 0 0;
+                 0 1 0 0 dt 0 0;
+                 0 0 1 0 0 dt 0;
+                 0 0 0 1 0 0 0;
+                 0 0 0 0 1 0 0;
+                 0 0 0 0 0 1 -dt * g;
+                 0 0 0 0 0 0 1];
+             
+            B = [zeros(3, 1); 1; zeros(3, 1)];
         end
         
-        function trans = transitions(Obj)
-            runway = Obj.Parameters.Runway;
-            height = Obj.Parameters.Height;
-            
-            n = runway * runway * height * 2;
-            m = 5;
-            
-            trans = zeros(n, n, m);
-            
-            for x = 1:runway
-                for ballx = 1:runway
-                    for bally = 1:height
-                        for ballvel = 1:2
-                            for input = 1:m
-                                if height > 1
-                                    switch input
-                                        case 1
-                                            next_state = {x - 1, ballx - ballvel, bally - 1, ballvel};
-                                        case 2
-                                            next_state = {x - 2, ballx - ballvel, bally - 1, ballvel};
-                                        case 3
-                                            next_state = {x + 1, ballx - ballvel, bally - 1, ballvel};
-                                        case 4
-                                            next_state = {x + 2, ballx - ballvel, bally - 1, ballvel};
-                                        case 5
-                                            next_state = {x, ballx - ballvel, bally - 1, ballvel};
-                                    end
-                                else
-                                    next_state = {x, ballx, bally, ballvel};
-                                end
-                                
-                                if next_state{1} < 1
-                                    next_state{1} = 1;
-                                end
-                                
-                                if next_state{1} > runway
-                                    next_state{1} = runway;
-                                end
-                                
-                                if next_state{2} < 1
-                                    next_state{2} = runway;
-                                end
-                                
-                                if next_state{3} < 1
-                                    next_state{3} = 1;
-                                end
-                                
-                                state_ind = sub2ind([runway runway height 2], x, ballx, bally, ballvel);
-                                next_state_ind = sub2ind([runway runway height 2], next_state{:});
-                                
-                                trans(next_state_ind, state_ind, input) = 1;
-                            end
-                        end
-                    end
-                end
-            end
+        function c = cost(Obj, State, Input, t)
+            c = Input' * Obj.Parameters.R(:, :, t) * Input;
         end
         
-        function output_given_state = sensor(Obj)
-            n = size(Obj.Transitions, 1);
-            runway = Obj.Parameters.Runway;
-            height = Obj.Parameters.Height;
-            
-            output_given_state = Obj.Parameters.MeasurementRate * eye(n);
-            
-            for i = 1:n
-                [x, ballx, bally, ballvel] = ind2sub([runway runway height 2], i);
-                inds = [];
-                
-                if ballvel == 1
-                    inds = sub2ind([runway runway height 2], x, ballx, bally, 2);
-                else
-                    inds = sub2ind([runway runway height 2], x, ballx, bally, 1);
-                end
-                
-                if x < runway
-                    inds = [inds; sub2ind([runway runway height 2], x + 1, ballx, bally, ballvel)];
-                end
-                
-                if x > 1
-                    inds = [inds; sub2ind([runway runway height 2], x - 1, ballx, bally, ballvel)];
-                end
-                
-                if ballx < runway
-                    inds = [inds; sub2ind([runway runway height 2], x, ballx + 1, bally, ballvel)];
-                end
-                
-                if ballx > 1
-                    inds = [inds; sub2ind([runway runway height 2], x, ballx - 1, bally, ballvel)];
-                end
-                
-                if bally < height
-                    inds = [inds; sub2ind([runway runway height 2], x, ballx, bally + 1, ballvel)];
-                end
-                
-                if bally > 1
-                    inds = [inds; sub2ind([runway runway height 2], x, ballx, bally - 1, ballvel)];
-                end
-                
-                output_given_state(inds, i) = (1 - Obj.Parameters.MeasurementRate) / length(inds);
-            end
+        function [Q, R] = quadraticize_cost(Obj, State, Input, t)
+            Q = Obj.Parameters.Q(:, :, t);
+            R = Obj.Parameters.R(:, :, t);
+        end
+        
+        function c = terminal_cost(Obj, State)
+            c = (State - Obj.Parameters.Goals(:, end))' * Obj.Parameters.Q(:, :, end) * (State - Obj.Parameters.Goals(:, end));
+        end
+        
+        function Q = quadraticize_terminal_cost(Obj, State)
+            Q = Obj.Parameters.Q(:, :, end);
         end
     end
     
